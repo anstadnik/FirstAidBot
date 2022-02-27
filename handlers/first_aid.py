621c3502645815b7e477cce7fd0f1f5edd3fa742
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-02-27 22:51:06 +0200

    Refactor, fixes

--- handlers/first_aid.py
+++ handlers/first_aid.py
@@ -1,18 +1,18 @@
-from typing import Callable
-
 import aiogram.utils.markdown as fmt
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from model.sheet import FiniteState, FiniteStateOptions, OptionalUrlAndStr
+from model.sheet import (FiniteState, FiniteStateOptions, OptionalUrlAndStr,
+                         get_data)
 
 
 class FirstAid(StatesGroup):
     dialog = State()
 
 
-def gen_keyboard(buttons: list[OptionalUrlAndStr]):
+def gen_keyboard(buttons: list[str]):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = [button.strip() for button in buttons]
     for keys in zip(buttons[::2], buttons[1::2]):
         keyboard.add(*keys)
     if len(buttons) % 2:
@@ -21,15 +21,39 @@ def gen_keyboard(buttons: list[OptionalUrlAndStr]):
     return keyboard
 
 
+async def answer(
+    message: types.Message,
+    state: FSMContext,
+    data: FiniteState,
+    msg: OptionalUrlAndStr,
+    parse_mode=None,
+    keyboard=None,
+):
+    if isinstance(msg, tuple):
+        link, msg = msg
+        await message.answer(fmt.hide_link(link), parse_mode=types.ParseMode.HTML)
+
+    try:
+        await message.answer(msg, parse_mode=parse_mode, reply_markup=keyboard)
+    except Exception as e:
+        await state.finish()
+        keyboard = types.ReplyKeyboardRemove()
+
+        kwargs = {"parse_mode": types.ParseMode.MARKDOWN}
+        await message.answer("Error!", reply_markup=keyboard, **kwargs)
+        await message.answer(f"The string: {msg}", **kwargs)
+        await message.answer(str(e), **kwargs)
+        await setup(message, state, data)
+        return True
+
+
 async def setup(message: types.Message, state: FSMContext, data: FiniteState):
     try:
         msg, possible_options = data
     except Exception as e:
         await state.finish()
-        await message.answer(
-            "Це ви щось погане зробили ваще, кличте програмістів",
-            reply_markup=types.ReplyKeyboardRemove(),
-        )
+        err = "Це ви щось погане зробили ваще, кличте програмістів"
+        await message.answer(err, reply_markup=types.ReplyKeyboardRemove())
         await message.answer(str(e), parse_mode=types.ParseMode.MARKDOWN)
         return
 
@@ -37,14 +61,7 @@ async def setup(message: types.Message, state: FSMContext, data: FiniteState):
 
     await state.set_data(possible_options)
     keyboard = gen_keyboard(list(possible_options.keys()))
-    if isinstance(msg, tuple):
-        link, msg = msg
-        await message.answer(fmt.hide_link(link), parse_mode=types.ParseMode.HTML)
-
-    try:
-        await message.answer(msg, reply_markup=keyboard)
-    except Exception as e:
-        await message.answer(str(e), parse_mode=types.ParseMode.MARKDOWN)
+    await answer(message, state, data, msg, keyboard=keyboard)
 
     await FirstAid.dialog.set()
 
@@ -52,7 +69,7 @@ async def setup(message: types.Message, state: FSMContext, data: FiniteState):
 async def process_input(message: types.Message, state: FSMContext, data: FiniteState):
     inp = message.text
 
-    if inp == "На початок":
+    if inp == "◀️На початок":
         await message.answer(
             r"Повертаємось\. Дякую, що допомагаєте, ви сонечко\! 🐞",
             reply_markup=types.ReplyKeyboardRemove(),
@@ -68,33 +85,15 @@ async def process_input(message: types.Message, state: FSMContext, data: FiniteS
     msg, possible_options = (await state.get_data())[inp]
 
     if possible_options is None:
-        try:
-            await message.answer(msg)
-        except Exception as e:
-            await state.finish()
-            keyboard = types.ReplyKeyboardRemove()
-            await message.answer(
-                str(e), reply_markup=keyboard, parse_mode=types.ParseMode.MARKDOWN
-            )
-            return await setup(message, state, data)
+        if await answer(message, state, data, msg) is True:
+            return
         return await setup(message, state, data)
 
     keyboard = gen_keyboard(list(possible_options.keys()))
     await state.update_data(possible_options)
 
-    if isinstance(msg, tuple):
-        link, msg = msg
-        await message.answer(fmt.hide_link(link), parse_mode=types.ParseMode.HTML)
-
-    try:
-        await message.answer(msg, reply_markup=keyboard)
-    except Exception as e:
-        await state.finish()
-        keyboard = types.ReplyKeyboardRemove()
-        await message.answer(
-            str(e), reply_markup=keyboard, parse_mode=types.ParseMode.MARKDOWN
-        )
-        return await setup(message, state, data)
+    if await answer(message, state, data, msg, keyboard=keyboard) is True:
+        return
 
 
 def get_handler(data: FiniteState, reset=False):
@@ -112,4 +111,4 @@ def get_handler(data: FiniteState, reset=False):
 
 def register_handlers_first_aid(dp: Dispatcher, data: FiniteState):
     dp.register_message_handler(get_handler(data, True), commands="start", state="*")
-    dp.register_message_handler(get_handler(data, False), state=FirstAid.dialog)
+    dp.register_message_handler(get_handler(data, False), state="*")
