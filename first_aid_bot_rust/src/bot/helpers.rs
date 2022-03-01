6eb0701f37779a2704c172059fcc0795b5c394f9
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-03-01 22:48:02 +0200

    Working bot!

---
+++ first_aid_bot_rust/src/bot/helpers.rs
@@ -0,0 +1,55 @@
+use teloxide::{
+    payloads::SendMessageSetters,
+    prelude2::*,
+    types::{KeyboardButton, KeyboardMarkup, ParseMode},
+};
+
+use crate::model::{FiniteState, FiniteStateOptions};
+
+pub async fn get_current_options<'a>(
+    data: &'a FiniteState,
+    context: &Vec<String>,
+) -> Option<&'a FiniteStateOptions> {
+    let mut current_state = data;
+    for choise in context {
+        current_state = &current_state.options.as_ref().unwrap().next_states[choise];
+    }
+    current_state.options.as_ref()
+}
+
+pub async fn make_keyboard(ordered_keys: &Vec<String>) -> KeyboardMarkup {
+    let mut keyboard: Vec<Vec<KeyboardButton>> = vec![];
+
+    for key_texts in ordered_keys.chunks(3) {
+        let row = key_texts
+            .iter()
+            .map(|key_text| KeyboardButton::new(key_text.as_str()))
+            .collect();
+
+        keyboard.push(row);
+    }
+
+    KeyboardMarkup::new(keyboard)
+}
+
+pub async fn send_message(
+    bot: &AutoSend<Bot>,
+    msg: &Message,
+    state: &FiniteState,
+) -> anyhow::Result<()> {
+    // TODO: Hide the link <28-02-22, astadnik> //
+    if let Some(link) = &state.link {
+        log::debug!("{}", link);
+        bot.send_message(msg.chat.id, format!("<a href='{link}'>&#8288;</a>"))
+            .parse_mode(ParseMode::Html)
+            .await?;
+    }
+    let msg = bot.send_message(msg.chat.id, &state.message);
+    if let Some(options) = &state.options {
+        msg.reply_markup(make_keyboard(&options.ordered_keys).await)
+    } else {
+        msg
+    }
+    .await?;
+    Ok(())
+}
--- first_aid_bot_rust/src/model/finite_state.rs
+++ first_aid_bot_rust/src/model/finite_state.rs
@@ -1,41 +1,28 @@
 use regex::Regex;
 use std::collections::HashMap;
 
-fn parse_link(link: &Option<String>) -> Option<String> {
+pub fn parse_link(link: &Option<String>) -> Option<String> {
+    dbg!(link);
     match link.to_owned() {
-        None => (None),
+        None => None,
         Some(link) if link.contains("file/d") => {
             let link = Regex::new(r".*file/d/").unwrap().replace(&link, "");
             let link = Regex::new(r"/.*").unwrap().replace(&link, "").to_string();
-            Some(link)
+            Some(format!("https://drive.google.com/uc?id={link}"))
         }
         Some(link) => Some(link),
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
-pub struct FiniteStateMsg {
-    pub link: Option<String>,
-    pub message: String,
+#[derive(Debug, Clone)]
+pub struct FiniteStateOptions {
+    pub ordered_keys: Vec<String>,
+    pub next_states: HashMap<String, FiniteState>,
 }
-
-impl FiniteStateMsg {
-    pub fn new(link: Option<String>, message: String) -> Self {
-        let link = parse_link(&link);
-        Self { link, message }
-    }
-}
-
-pub type FiniteStateOptions = Option<HashMap<String, FiniteState>>;
 
 #[derive(Debug, Clone)]
 pub struct FiniteState {
-    pub message: FiniteStateMsg,
-    pub options: FiniteStateOptions,
-}
-
-impl FiniteState {
-    pub fn new(message: FiniteStateMsg, options: FiniteStateOptions) -> Self {
-        Self { message, options }
-    }
+    pub link: Option<String>,
+    pub message: String,
+    pub options: Option<FiniteStateOptions>,
 }
