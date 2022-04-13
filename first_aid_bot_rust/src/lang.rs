da5a4ea8a60440035062844266568709f095c264
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-04-13 10:20:57 +0300

    Replace serializing of enum with string

--- first_aid_bot_rust/src/lang.rs
+++ first_aid_bot_rust/src/lang.rs
@@ -24,6 +24,9 @@ impl Lang {
             Ru => RU_LD, */
         }
     }
+    pub fn name(self) -> String {
+        self.details().name.to_string()
+    }
 }
 
 impl TryFrom<&str> for Lang {
@@ -31,9 +34,9 @@ impl TryFrom<&str> for Lang {
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         match value {
-            "Ukrainian" => Ok(Ua),
-            /* "English" => Ok(En),
-            "Russian" => Ok(Ru), */
+            UA_STR => Ok(Ua),
+            /* EN_STR => Ok(En),
+            RU_STR => Ok(Ru), */
             wtf => Err(format!("Cannot create a language from {wtf}")),
         }
     }
@@ -57,24 +60,27 @@ pub struct LangDetails {
     pub greeting: &'static str,
 }
 
+const UA_STR: &str = "Ukrainian";
 const UA_LD: LangDetails = LangDetails {
-    name: "Ukrainian",
+    name: UA_STR, 
     button_text: "Українська",
     error: "Сталась помилка, будь ласка, повідомте про це у https://t.me/+SvnzzsxStydmNGI6",
     use_buttons_text: "Використайте кнопки, або перезавантажте бота (/start)",
     greeting: "Що трапилось?",
 };
 
-/* const EN_LD: LangDetails = LangDetails {
-    name: "English",
+/* const EN_STR: &str = "Ukrainian";
+const EN_LD: LangDetails = LangDetails {
+    name: EN_STR,
     button_text: "English",
     error: "An error occured, please tell us about it at https://t.me/+SvnzzsxStydmNGI6",
     use_buttons_text: "Use buttons, or restart bot if something is wrong (/start)",
     greeting: "What happened?",
 };
 
+const RU_STR: &str = "Ukrainian";
 const RU_LD: LangDetails = LangDetails {
-    name: "Russian",
+    name: RU_STR,
     button_text: "Русский",
     error: "Произошла ошибка, пожалуйста, сообщине о ней в https://t.me/+SvnzzsxStydmNGI6",
     use_buttons_text: "Используйте кнопки, или перезагрузите бота, если что-то сломалось (/start)",
