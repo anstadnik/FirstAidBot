eb38e05264495d8863c5ce0ff64085e010ac05cd
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-09-02 17:31:56 +0300

    Fixes

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -9,6 +9,8 @@ pub mod prelude {
     pub use super::lang::Lang;
 }
 
+use std::{env, fs::File, io};
+
 use anyhow::{anyhow, Context};
 use bytes::Buf;
 use csv::Reader;
@@ -16,15 +18,20 @@ use futures::{stream, StreamExt, TryStreamExt};
 use indexmap::IndexMap;
 use prelude::*;
 
-use crate::{SHEET_ID_PROD, SHEET_ID_TEST};
-
 use self::finite_state::Row;
 
-async fn get_rows(sheet_id: &str, sheet_name: String) -> anyhow::Result<Vec<Row>> {
-    let url = format!(
+async fn get_rows(filename: Option<&str>, sheet_name: String) -> anyhow::Result<Vec<Row>> {
+    let rdr: Box<dyn io::Read> = match filename {
+        Some(filename) => Box::new(io::BufReader::new(File::open(filename)?)),
+        None => {
+            let sheet_id = env::var("SHEET_ID")?;
+            let url = format!(
         "https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
     );
-    let rdr = Reader::from_reader(reqwest::get(url).await?.bytes().await?.reader());
+            Box::new(reqwest::get(url).await?.bytes().await?.reader())
+        }
+    };
+    let rdr = Reader::from_reader(rdr);
     rdr.into_deserialize()
         .collect::<Result<_, _>>()
         .context("Cannot parse csv")
@@ -51,13 +58,8 @@ fn get_next_states_for_key(data: &[Row], key: &str) -> anyhow::Result<FSNextStat
         .collect::<anyhow::Result<IndexMap<String, FS>>>()
 }
 
-async fn get_finite_state(lang: Lang) -> anyhow::Result<FS> {
-    let sheet_id = if cfg!(debug_assertions) {
-        SHEET_ID_TEST
-    } else {
-        SHEET_ID_PROD
-    };
-    let mut rows = get_rows(sheet_id, lang.name()).await?;
+async fn get_finite_state(lang: Lang, filename: Option<&str>) -> anyhow::Result<FS> {
+    let mut rows = get_rows(filename, lang.name()).await?;
     rows.retain(|record| !record.is_empty());
     for row in &mut rows {
         row.key = row.key.trim().to_string();
@@ -65,9 +67,9 @@ async fn get_finite_state(lang: Lang) -> anyhow::Result<FS> {
     Ok(FS::entry(&lang, get_next_states_for_key(&rows, "")?))
 }
 
-pub async fn get_data() -> anyhow::Result<MultilangStates> {
+pub async fn get_data(filename: Option<&str>) -> anyhow::Result<MultilangStates> {
     stream::iter(Lang::iter())
-        .then(|lang| async move { get_finite_state(lang).await.map(|fs| (lang, fs)) })
+        .then(|lang| async move { get_finite_state(lang, filename).await.map(|fs| (lang, fs)) })
         .try_collect()
         .await
 }
--- src/model/test.rs
+++ src/model/test.rs
@@ -3,5 +3,5 @@ use super::*;
 // TODO: add messages test when https://github.com/teloxide/teloxide/issues/702 is resolved
 #[tokio::test]
 async fn test_data_loading() {
-    assert!(get_data().await.is_ok())
+    assert!(get_data(None).await.is_ok())
 }
