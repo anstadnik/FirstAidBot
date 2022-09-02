838a72dbe104c923a12aa11d1fe170ca71331b0b
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-09-02 17:31:56 +0300

    Fixes

--- src/model/data.rs
+++ src/model/data.rs
@@ -16,10 +16,14 @@ impl Data {
     pub async fn cached() -> Self {
         info!("Cached data!");
         Self {
-            data: Some(get_data().await.unwrap()),
+            data: Some(get_data(Some("table.csv")).await.unwrap()),
         }
     }
-    pub async fn get<'a>(&'a self, lang: Lang, context: &'_ [String]) -> anyhow::Result<Cow<'a, FS>> {
+    pub async fn get<'a>(
+        &'a self,
+        lang: Lang,
+        context: &'_ [String],
+    ) -> anyhow::Result<Cow<'a, FS>> {
         let state = match &self.data {
             Some(data) => Cow::Borrowed(
                 data.get(&lang)
@@ -27,7 +31,7 @@ impl Data {
                     .get_state(context)?,
             ),
             None => Cow::Owned(
-                get_data()
+                get_data(None)
                     .await?
                     .get(&lang)
                     .ok_or_else(|| Error::msg("No such lang {lang}"))?
