934e8a5e47af773d38d842ba0bcf4dd5c48127ab
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-09-07 11:12:22 +0300

    WIP refactor

--- src/bot/helpers.rs
+++ src/bot/helpers.rs
@@ -1,9 +1,5 @@
 use super::prelude::*;
-use crate::REDIS_URLS;
-use futures::future::join_all;
 use itertools::Itertools;
-use redis::{aio::MultiplexedConnection, Client};
-use teloxide::dispatching::dialogue::{serializer::Bincode, RedisStorage};
 use teloxide::utils::markdown::escape;
 
 fn split_msg(msg: &str) -> impl Iterator<Item = String> {
@@ -30,14 +26,4 @@ pub async fn send_plain_string(bot: &FABot, id: ChatId, msg: &str) -> anyhow::Re
         bot.send_message(id, msg).await?;
     }
     Ok(())
-}
-
-pub async fn connect_to_redis() -> (MultiplexedConnection, Arc<FirstAidStorage>) {
-    let results = join_all(REDIS_URLS.into_iter().map(|url| async move {
-        let connection = Client::open(url)?
-            .get_multiplexed_tokio_connection()
-            .await?;
-        anyhow::Ok((connection, RedisStorage::open(url, Bincode).await?))
-    }));
-    results.await.into_iter().find_map(Result::ok).unwrap()
 }
---
+++ src/bot/run.rs
@@ -0,0 +1,50 @@
+use super::dialogue::prelude::*;
+use super::prelude::*;
+use crate::bot::error_handler::FAErrorHandler;
+use crate::REDIS_URLS;
+use futures::future::join_all;
+use redis::{aio::MultiplexedConnection, Client};
+use teloxide::adaptors::throttle::Limits;
+use teloxide::dispatching::dialogue::{serializer::Bincode, RedisStorage};
+use teloxide::types::ParseMode;
+use teloxide::utils::command::BotCommands;
+
+pub async fn connect_to_redis() -> (MultiplexedConnection, Arc<FirstAidStorage>) {
+    let results = join_all(REDIS_URLS.into_iter().map(|url| async move {
+        let connection = Client::open(url)?
+            .get_multiplexed_tokio_connection()
+            .await?;
+        anyhow::Ok((connection, RedisStorage::open(url, Bincode).await?))
+    }));
+    results.await.into_iter().find_map(Result::ok).unwrap()
+}
+
+pub async fn run_bot(data: Data) {
+    log::info!("Starting dialogue_bot...");
+
+    let bot = Bot::from_env()
+        .throttle(Limits::default())
+        .parse_mode(ParseMode::MarkdownV2)
+        .auto_send();
+
+    bot.set_my_commands(FACommands::bot_commands())
+        .await
+        .unwrap();
+
+    let (redis_con, storage) = connect_to_redis().await;
+
+    let handler = Update::filter_message()
+        .branch(get_commands_branch())
+        .branch(get_maintainer_commands_branch())
+        .enter_dialogue::<Message, FirstAidStorage, State>()
+        .branch(dptree::case![State::Start { lang }].endpoint(start_handler))
+        .branch(dptree::case![State::Dialogue { lang, context }].endpoint(handle_dialogue));
+
+    Dispatcher::builder(bot.clone(), handler)
+        .dependencies(dptree::deps![Arc::new(data), redis_con, storage])
+        .error_handler(FAErrorHandler::new(bot))
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
+}
