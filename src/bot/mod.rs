9ad3e834f2c1cf1cc174583d94441b87551a3e4d
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-09-07 11:22:13 +0300

    WIP refactor

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -1,6 +1,5 @@
 mod dialogue;
 mod error_handler;
-mod helpers;
 mod run;
 mod state;
 
@@ -8,7 +7,7 @@ mod prelude {
     use teloxide::adaptors::{DefaultParseMode, Throttle};
     use teloxide::dispatching::dialogue::{serializer::Bincode, RedisStorage};
 
-    pub use crate::bot::helpers::send_plain_string;
+    pub use crate::bot::error_handler::send_plain_string;
     pub use crate::model::prelude::*;
     pub use std::sync::Arc;
     pub use teloxide::prelude::*;
@@ -17,6 +16,6 @@ mod prelude {
     pub type FADialogue = Dialogue<State, RedisStorage<Bincode>>;
     pub type FABot = AutoSend<DefaultParseMode<Throttle<Bot>>>;
     pub type FirstAidStorage = RedisStorage<Bincode>;
-} /* prelude */
+}
 
 pub use run::run_bot;
