650d719ca57a0a21b112519b41c805aba5a2e8eb
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-09-08 12:56:35 +0300

    Refactor

--- src/bot/dialogue/mod.rs
+++ src/bot/dialogue/mod.rs
@@ -1,15 +1,10 @@
 mod commands;
+mod fa_args;
 mod handlers;
 mod helpers;
 mod keyboard;
-mod logic;
 
 pub mod prelude {
-    // pub use super::handlers::{start_handler, FAMsgArgs};
-    // pub use super::helpers::{log_to_redis, send_state};
-    // pub use super::keyboard::make_keyboard_from_state;
-    // pub use super::logic::{move_to_state, state_transition};
-    // pub use crate::bot::prelude::*;
     pub use super::commands::{get_commands_branch, get_maintainer_commands_branch, FACommands};
     pub use super::handlers::{handle_dialogue, start_handler};
 }
