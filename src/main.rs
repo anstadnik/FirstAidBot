390203c8f6a5610947c23bcc0e7cfcdd59b59913
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-10-13 12:03:08 +0300

    Alia changed the nickname

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use model::prelude::Data;
 use teloxide::types::UserId;
 
 const MAINTAINER_IDS: [teloxide::types::UserId; 1] = [UserId(131596643)];
-const MAINTAINER_USERNAMES: [&str; 2] = ["astadnik", "AL_Lev"];
+const MAINTAINER_USERNAMES: [&str; 2] = ["astadnik", "Oleksa_Lev"];
 const HELP_CHAT_URL: &str = "https://t.me/+VOd-pnzhWvU2Yjg6";
 const REDIS_URLS: [&str; 2] = ["redis://redis:6379", "redis://127.0.0.1:6379"];
 const REDIS_USERS_SET_KEY: &str = "all_users";
