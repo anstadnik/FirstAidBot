971965dfe7d221105ef54b71a2b782f1d7471712
Stadnik Andrii <stadnikandriy1@gmail.com>
2023-01-08 11:08:07 +0200

    --wip-- [skip ci]

---
+++ core/src/bridge_generated.web.rs
@@ -0,0 +1,15 @@
+use super::*;
+// Section: wire functions
+
+#[wasm_bindgen]
+pub fn wire_hello_world(port_: MessagePort) {
+    wire_hello_world_impl(port_)
+}
+
+// Section: allocate functions
+
+// Section: related functions
+
+// Section: impl Wire2Api
+
+// Section: impl Wire2Api for JsValue
